d9674a5ecbbaf44f0d1edbf4d6b864e90245e972
Andrey <hello.andrey@gmail.com>
2013-04-17 22:50:26 +0200

    fixed gh pages

--- demo/preload.js
+++ demo/preload.js
@@ -1,15 +1,8 @@
 function loadSampleKB(){
-<<<<<<< HEAD:preload.js
 	var desc = 'It is a demo application that demonstrates features of VIE, a semantics interaction engine.';
 	var purp = 'The article, which is used for the demo, contains annotated elements - entities. The VIE widget Image Search uses that annotations for querying images to the FLICKR image database.';
 	var use = 'All the entities are highlighted in dark blue. You can click an entity to start image search. If any image will be found, it will be placed below the article.';
 	var start = 'To start working with the application, choose the entity, for which you would like to find related images.';
-=======
-	var desc = 'It is a demo application, which demonstrates features of VIE - a semantics interaction engine.';
-	var purp = 'The article, used for the demo, contains annotated elements - entities. The VIE widged uses that semantics for querying images to FLICKR image database.';
-	var use = 'All entities are highlighted with dark blue. You can click an entity to start image search. If any image will be found, it will be placed below the article.';
-	var start = 'To start working with the application choose the entity, for which you would like to find related images.';
->>>>>>> origin/master:demo/preload.js
 	kbAPI.staticKB.addRecord({
 		elementType: 'main',
 		title: 'VIE Widget Image Search',
@@ -20,11 +13,7 @@ function loadSampleKB(){
 	});
 	
 	desc = 'This annotated element represents an entity.';
-<<<<<<< HEAD:preload.js
 	purp = 'Hidden semantics allows to get more information about the entity, than it\'s shown in text. For example, the widget uses entity type (such as "Person", "Place") and its attributes to search for related images.';
-=======
-	purp = 'Hidded semantics allows to get more information about the entity, than it\'s shawn in text. For example, use entity type (such as "Person", "Place") and its attributes to search for related images.';
->>>>>>> origin/master:demo/preload.js
 	use = 'You can click the entity to start image search. If any image will be found, it will be placed below the article.';
 	kbAPI.staticKB.addRecord({
 		elementType: 'annotated',
@@ -37,15 +26,9 @@ function loadSampleKB(){
 	kbAPI.staticKB.addRecord({
 		elementType: 'result',
 		title: 'a result',
-<<<<<<< HEAD:preload.js
 		description: 'This result is a thumbnail (a small version) of the image that has been found in the FLICKR image database.',
 		purpose: 'The image is relevant to the clicked entity.',
 		use: 'You can click it to open a full-sized version at the original location of the image.'
-=======
-		description: 'This result is a thumbnail (a small version) of the image that has been found in FLICKR image database.',
-		purpose: 'The image is relevant to the clicked entity.',
-		use: 'You can click it to open the full-sized version at the original location of the image.'
->>>>>>> origin/master:demo/preload.js
 	});
 	
 	kbAPI.templates.addRecord({
@@ -75,11 +58,7 @@ function loadSampleKB(){
 	
 	kbAPI.templates.addRecord({
 		id: 'How_to_start',
-<<<<<<< HEAD:preload.js
 		label: 'How do I start?',
-=======
-		label: 'How to start?',
->>>>>>> origin/master:demo/preload.js
 		types : ["main"],
 		category: "how",
 		context: [
@@ -107,8 +86,6 @@ function loadSampleKB(){
 					[{value:'The following events sequence has been passed:',type:'manual'},{type: 'reference',value: 'trace'}]
 				]
 	});
-<<<<<<< HEAD:preload.js
-=======
 	
 	kbAPI.templates.addRecord({
 	id: 'What_is_its_relevance',
@@ -119,5 +96,4 @@ function loadSampleKB(){
 				[{value:'Its relevance is:',type:'manual'},{type: 'reference',value: 'relevance'}]
 			]
 	});
->>>>>>> origin/master:demo/preload.js
 }
